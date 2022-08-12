f63b345b3d92d207f39b7dd02980904bf380c298
Igor Grankin <i.grankin@dodopizza.com>
2022-08-12 12:38:05 +0300

    Initial Commit

---
+++ Greed kata/Greed_kata.h
@@ -0,0 +1,18 @@
+//
+//  Greed_kata.h
+//  Greed kata
+//
+//  Created by Игорь Гранкин on 12.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Greed_kata.
+FOUNDATION_EXPORT double Greed_kataVersionNumber;
+
+//! Project version string for Greed_kata.
+FOUNDATION_EXPORT const unsigned char Greed_kataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Greed_kata/PublicHeader.h>
+
+
