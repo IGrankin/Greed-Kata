9a8f4332d1ac89ffac063789f137ff4ead2c7d81
Igor Grankin <i.grankin@dodopizza.com>
2022-08-12 15:26:11 +0300

    9: red test and renaming

--- Greed kataTests/Greed_kataTests.swift
+++ Greed kataTests/Greed_kataTests.swift
@@ -17,7 +17,7 @@ class Greed_kataTests: XCTestCase {
         sut = Calculator()
     }
     
-    func test_ZeroMovesReturnsZeroPoints() {
+    func test_zeroMovesReturnsZeroPoints() {
         let result = sut.calculate([])
         XCTAssertEqual(result, 0)
     }
@@ -32,7 +32,7 @@ class Greed_kataTests: XCTestCase {
         XCTAssertEqual(result, 50)
     }
     
-    func test_Not1And5NumbersReturnsZeroPoints() {
+    func test_not1And5NumbersReturnsZeroPoints() {
         let result = sut.calculate([2,3,4,2,3])
         XCTAssertEqual(result, 0)
     }
@@ -43,43 +43,50 @@ class Greed_kataTests: XCTestCase {
     }
     
     // MARK: - triples
-    func test_Triple1Returns1000Points() {
+    func test_triple1Returns1000Points() {
         let result = sut.calculate([1,1,1])
         XCTAssertEqual(result, 1000)
     }
     
-    func test_Triple2Returns200Points() {
+    func test_triple2Returns200Points() {
         let result = sut.calculate([2,2,2])
         XCTAssertEqual(result, 200)
     }
     
-    func test_Triple3Returns300Points() {
+    func test_triple3Returns300Points() {
         let result = sut.calculate([3,3,3])
         XCTAssertEqual(result, 300)
     }
     
-    func test_Triple4Returns400Points() {
+    func test_triple4Returns400Points() {
         let result = sut.calculate([4,4,4])
         XCTAssertEqual(result, 400)
     }
     
-    func test_Triple5Returns500Points() {
+    func test_triple5Returns500Points() {
         let result = sut.calculate([5,5,5])
         XCTAssertEqual(result, 500)
     }
     
-    func test_Triple6Returns600Points() {
+    func test_triple6Returns600Points() {
         let result = sut.calculate([6,6,6])
         XCTAssertEqual(result, 600)
     }
     
-    func test_Triple7ReturnsZeroPoints() {
+    func test_triple7ReturnsZeroPoints() {
         let result = sut.calculate([7,7,7])
         XCTAssertEqual(result, 0)
     }
     
-    func test_TripleZerosReturnsZeroPoints() {
+    func test_tripleZerosReturnsZeroPoints() {
         let result = sut.calculate([0,0,0])
         XCTAssertEqual(result, 0)
+    }
+    
+    // MARK: - sum checks
+    
+    func test_triple1And1And5Returns1150Points() {
+        let result = sut.calculate([1,1,1,1,5])
+        XCTAssertEqual(result, 1150)
     }
 }
